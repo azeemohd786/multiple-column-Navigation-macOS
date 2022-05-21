71c7651af5eba62d20c8948e34bbaef53cf17aa8
Azeem <azeemireland@gmail.com>
2022-05-21 10:08:28 +0530

    Initial Commit

---
+++ macOSColumnTrial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  macOSColumnTrial
+//
+//  Created by Azeem Azeez on 21/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ macOSColumnTrial/macOSColumnTrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOSColumnTrialApp.swift
+//  macOSColumnTrial
+//
+//  Created by Azeem Azeez on 21/05/22.
+//
+
+import SwiftUI
+
+@main
+struct macOSColumnTrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
